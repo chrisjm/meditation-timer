c391b776f13b1b6f2489f6ed962f0736d543d13f
Chris Mears <chris.mears@gmail.com>
2025-02-14 23:10:38 -0800

    Update ToggleSwitch

--- src/lib/components/ToggleSwitch.svelte
+++ src/lib/components/ToggleSwitch.svelte
@@ -16,13 +16,24 @@
 			{checked}
 			on:change={onChange}
 			on:keydown={(e) => e.key === 'Enter' && onChange(e)}
-			class="peer sr-only"
+			class="sr-only"
 			role="switch"
 			aria-checked={checked}
 			tabindex="0"
 		/>
 		<div
-			class="peer h-6 w-11 rounded-full bg-gray-200 peer-checked:bg-blue-600 peer-focus:ring-4 peer-focus:ring-blue-300 peer-focus:outline-none after:absolute after:top-[2px] after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:after:translate-x-full peer-checked:after:border-white dark:border-gray-600 dark:bg-gray-700 dark:peer-focus:ring-blue-800"
-		></div>
+			class="relative h-6 w-11 rounded-full bg-gray-200 transition-colors duration-200 ease-in-out focus-within:ring-4 focus-within:ring-blue-300 dark:bg-gray-700 dark:focus-within:ring-blue-800"
+			class:bg-blue-600={checked}
+			class:dark:bg-blue-500={checked}
+		>
+			<div
+				class="absolute h-5 w-5 transform rounded-full border bg-white transition-transform duration-200 ease-in-out dark:border-gray-600"
+				class:border-gray-300={!checked}
+				class:border-blue-600={checked}
+				class:dark:border-gray-500={!checked}
+				class:dark:border-blue-500={checked}
+				style="top: 2px; left: 2px; transform: translateX({checked ? '20px' : '0px'});"
+			></div>
+		</div>
 	</label>
 </div>
