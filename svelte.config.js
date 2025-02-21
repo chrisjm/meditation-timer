d9ca82a7eef218db910b784eabeffe21d2c39bec
Chris Mears <chris.mears@gmail.com>
2025-02-21 12:42:32 -0800

    Add Cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel';
+import vercel from '@sveltejs/adapter-vercel';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
+
+// Choose adapter based on environment variable
+const adapter = process.env.ADAPTER === 'cloudflare' ? cloudflare : vercel;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
