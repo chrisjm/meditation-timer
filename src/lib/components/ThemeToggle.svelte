ac4d463b20c7eacd05882fa53df6315c61f15639
Chris Mears <chris.mears@gmail.com>
2025-02-21 19:17:41 -0800

    Update icons to Lucide icons

--- src/lib/components/ThemeToggle.svelte
+++ src/lib/components/ThemeToggle.svelte
@@ -2,6 +2,7 @@
 	import { timerSettings } from '$lib/stores/timerSettings';
 	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
+	import { Sun, Moon, SunMoon } from 'lucide-svelte';
 
 	// Handle theme toggle - cycles through light -> dark -> auto
 	const handleToggleTheme = () => {
@@ -66,44 +67,11 @@
 		tabindex="0"
 	>
 		{#if $timerSettings.theme === 'light'}
-			<!-- Sun icon for light mode -->
-			<svg
-				class="h-5 w-5"
-				fill="currentColor"
-				viewBox="0 0 20 20"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
-				></path>
-			</svg>
+			<Sun class="h-5 w-5" />
 		{:else if $timerSettings.theme === 'dark'}
-			<!-- Moon icon for dark mode -->
-			<svg
-				class="h-5 w-5"
-				fill="currentColor"
-				viewBox="0 0 20 20"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-			</svg>
+			<Moon class="h-5 w-5" />
 		{:else}
-			<!-- Computer icon for auto mode -->
-			<svg
-				class="h-5 w-5"
-				fill="currentColor"
-				viewBox="0 0 20 20"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					d="M13 7H7v6h6V7z"
-				></path>
-				<path
-					fill-rule="evenodd"
-					d="M7 2a2 2 0 00-2 2v12a2 2 0 002 2h6a2 2 0 002-2V4a2 2 0 00-2-2H7zm6 2H7v12h6V4z"
-					clip-rule="evenodd"
-				></path>
-			</svg>
+			<SunMoon class="h-5 w-5" />
 		{/if}
 	</button>
 </div>
