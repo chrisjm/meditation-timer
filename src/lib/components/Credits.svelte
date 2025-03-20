51c938562b13d7560c0ec3abcbf3c4c4604e73c6
Chris Mears <chris.mears@gmail.com>
2025-03-19 19:16:53 -0700

    [Feature] Add Kirtan Kriya link

--- src/lib/components/Credits.svelte
+++ src/lib/components/Credits.svelte
@@ -26,9 +26,16 @@
 			Chris J Mears
 		</a>
 	</p>
+	<p class="text-xs text-gray-500 dark:text-gray-400">
+		✨ Check out the <a
+			href="https://www.kirtan-kriya-timer.com/"
+			target="_blank"
+			class="hover:text-gray-700 dark:hover:text-gray-200">Kirtan Kriya Timer</a
+		> 🧘‍♂️
+	</p>
 	<button
 		type="button"
-		class="text-xs text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200 cursor-pointer"
+		class="cursor-pointer text-xs text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
 		onclick={handleOpen}
 		onkeydown={(e) => e.key === 'Enter' && handleOpen()}
 	>
